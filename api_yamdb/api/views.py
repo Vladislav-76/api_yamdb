b06923a9c080c0134bbc61c11f88c8c74f5cee94
Константин Чечет <chechet.k@gmail.com>
2022-07-28 03:44:30 +0300

    Добавил создание пользователя(auth без токена), Доделать пермищен для юзера

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,13 +3,21 @@ from reviews.models import User
 from .serializers import AuthSignupSerializer, UserSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import filters
+
+
+
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    #permission_classes = (permissions.IsAdminUser, )
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('username',)
+    permission_classes = (permissions.IsAdminUser, )
 
 
 @api_view(['POST'])
@@ -18,5 +26,8 @@ def user_create(request):
         serializer = AuthSignupSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            print(serializer)
+            #token = default_token_generator.make_token(serializer)
+            #uid = urlsafe_base64_encode(force_bytes(serializer.pk))
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -28,7 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',     # Новая строчка
-    'djoser',                       # Новая строчка
+    'djoser',
+    'django_filters',                       # Новая строчка
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
 }
 
 
